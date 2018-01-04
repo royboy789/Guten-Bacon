267badb2256801c3e24672e83007a0242ce68aed
Josh Pollock <josh@calderawp.com>
2018-01-03 19:20:28 -0500

    initial block commit

---
+++ ex2-react.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Learn Gutenberg Example 2: React
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     text-domain
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ */
+include_once  __DIR__ . '/blocks/blocks.php';
+
